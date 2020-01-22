ab3de84cc518e25778f278a469a6f611286d3aba
agil <antonio_g_s@hotmail.com>
2020-01-22 23:12:03 +0100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Antonio Gil Sánchez on 22/01/2020.
+//  Copyright © 2020 Antonio Gil Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
